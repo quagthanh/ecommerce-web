9a55133a8bb80396b52fe3323ae1a2436ff5da90
quagthanh <133523413+quagthanh@users.noreply.github.com>
2024-09-04 22:51:09 +0700

    repush shop women men kid ui

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -3,12 +3,12 @@ import all_product from '../Components/Assets/all_product'
 
 export const ShopContext=createContext(null);
 
-const ShopContextProvider=()=>{
-    const all_product_value=all_product;
+const ShopContextProvider=(props)=>{
+    const all_product_value={all_product};
     return (
         <ShopContext.Provider value={all_product_value}>
             {props.children}
         </ShopContext.Provider>
     )
 }
-export default ShopContext;
+export default ShopContextProvider;
